094c118c2dac8daedbfd2d19e445a61754e3cea7
shinlt1108 <shinlt1108@naver.com>
2025-04-06 13:36:11 +0900

    Create 2753.c

---
+++ 2nd_week/2753.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void)
+{
+    int a;
+    scanf("%d", &a);
+    if ( a%4 == 0 && a%100 != 0 )
+        printf("1\n");
+    else if ( a%400 == 0 )
+        printf("1\n");
+    else
+        printf("0\n");
+    return 0;
+}
